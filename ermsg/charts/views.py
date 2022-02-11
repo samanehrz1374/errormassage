b5f56346c35fea6383a1e4b5931f0154ef65fd5f
samaneh rezaie <samaneh.rezaie74@gmail.com>
2022-02-11 22:59:06 +0330

    Add data from voldatabase to db.html

--- ermsg/charts/views.py
+++ ermsg/charts/views.py
@@ -25,16 +25,20 @@ def charts(request):
     return render(request,'charts/charts.html',context=mydict)
 
 def db(request):
-    charts=VolDatabase.objects.all().values()
-    # df=time.time()
-    # df1=datetime.fromtimestamp(df)
-    # Df=VolumeTable.objects.get('created_at').values()
-    # times=datetime.fromtimestamp(df)
+    # charts=VolDatabase.objects.all().values()
+    labels = []
+    data = []
 
+    queryset = VolDatabase.objects.order_by('-created_at_int_datetime')
+    for city in queryset:
+        labels.append(city.total_volumerls)
+        data.append(city.created_at_int_datetime)
     context={
-        'results':charts,
+        # 'results':charts,
         # 'df':df,
         # 'df1':df1
+        'labelss': labels,
+        'dataa': data,
     }
     return render(request,'charts/db.html',context)
     
